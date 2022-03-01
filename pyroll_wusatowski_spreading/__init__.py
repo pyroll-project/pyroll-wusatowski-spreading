d5e9941565914c0f77f236eeef1bac9afaaff49a
Max Weiner <max.weiner@imf.tu-freiberg.de>
2022-03-01 11:36:26 +0100

    fixes

---
+++ pyroll_wusatowski_spreading/__init__.py
@@ -0,0 +1,2 @@
+from . import hookspecs
+from . import hookimpls
--- pyroll_wusatowski_spreading/hookimpls.py
+++ pyroll_wusatowski_spreading/hookimpls.py
@@ -1,7 +1,6 @@
 import sys
 
 from pyroll import RollPass
-from . import lib
 
 
 @RollPass.hookimpl
@@ -11,7 +10,8 @@ def wusatowski_temperature_coefficient(roll_pass):
 
 @RollPass.hookimpl
 def wusatowski_velocity_coefficient(roll_pass):
-    compression = lib.compression(roll_pass)
+    compression = (roll_pass.ideal_out_profile.equivalent_rectangle.height
+                   / roll_pass.in_profile.equivalent_rectangle.height)
     return (-0.002958 + 0.00341 * compression) * roll_pass.velocity + 1.07168 - 0.10431 * compression
 
 
@@ -27,30 +27,29 @@ def wusatowski_friction_coefficient(roll_pass):
 
 @RollPass.hookimpl
 def wusatowski_exponent(roll_pass):
-    in_equivalent_height = lib.equivalent_height(roll_pass.in_profile)
-    in_equivalent_width = lib.equivalent_width(roll_pass.in_profile)
+    in_equivalent_height = roll_pass.in_profile.equivalent_rectangle.height
+    in_equivalent_width = roll_pass.in_profile.equivalent_rectangle.width
     return 10 ** (
-            -1.269 * (in_equivalent_height / 2 / roll_pass.roll_radius) ** 0.56
+            -1.269 * (in_equivalent_height / (2 * roll_pass.roll_radius)) ** 0.56
             * in_equivalent_width / in_equivalent_height
     )
 
 
 @RollPass.hookimpl
-def width_change(roll_pass):
-    compression = lib.compression(roll_pass)
+def width_change(roll_pass: RollPass):
+    compression = (roll_pass.ideal_out_profile.equivalent_rectangle.height
+                   / roll_pass.in_profile.equivalent_rectangle.height)
     spread = (
             roll_pass.wusatowski_temperature_coefficient
             * roll_pass.wusatowski_velocity_coefficient
-            * wusatowski_material_coefficient
-            * wusatowski_friction_coefficient
-            * compression ** (-wusatowski_exponent)
+            * roll_pass.wusatowski_material_coefficient
+            * roll_pass.wusatowski_friction_coefficient
+            * compression ** (-roll_pass.wusatowski_exponent)
     )
-    out_equivalent_height = lib.equivalent_height(roll_pass.out_profile)
+    out_equivalent_width = spread * roll_pass.in_profile.equivalent_rectangle.width
 
-    in_equivalent_width = lib.equivalent_width(roll_pass.in_profile)
-    out_equivalent_width = spread * in_equivalent_width
-
-    out_profile_width = out_equivalent_width * roll_pass.out_profile.height / out_equivalent_height
+    out_profile_width = (out_equivalent_width * roll_pass.ideal_out_profile.height
+                         / roll_pass.ideal_out_profile.equivalent_rectangle.height)
     width_change = out_profile_width - roll_pass.in_profile.rotated.width
 
     return width_change
