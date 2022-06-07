87044d369934d8f8041b2e03f5cb4c449ee3cbdd
Max Weiner <max.weiner@imf.tu-freiberg.de>
2022-06-07 17:05:00 +0200

    remove min test since overfilled

--- tests/test_solve.py
+++ tests/test_solve.py
@@ -24,24 +24,3 @@ def test_solve_trio(tmp_path: Path, caplog):
 
     print()
     print(caplog.text)
-
-
-def test_solve_min(tmp_path: Path, caplog):
-    caplog.set_level(logging.ERROR, "matplotlib")
-    caplog.set_level(logging.DEBUG)
-
-    import pyroll.wusatowski_spreading
-
-    from pyroll.ui.cli.res import input_min
-
-    pyroll.core.solve(input_min.sequence, input_min.in_profile)
-
-    report = pyroll.ui.Reporter().render(input_min.sequence)
-
-    report_file = tmp_path / "report.html"
-    report_file.write_text(report)
-    print()
-    print(report_file)
-
-    print()
-    print(caplog.text)
